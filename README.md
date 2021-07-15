c22d5068d6cc1ad6f1eb446fab8dab152466a153
unknown <ruro122020@gmail.com>
2021-07-15 13:01:10 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Backend to tracking-patient-status
+Technologies use: express and socket.io
+To start server use command nodemon
---
+++ socketio/socketio.js
@@ -0,0 +1,24 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const PORT = process.env.PORT || 3000;
+const option = {
+  cors: {
+    origin: "http://localhost:3001",
+    method: ["GET", "POST"],
+  },
+};
+
+const io = require("socket.io")(server, option);
+
+io.on("connection", (socket) => {
+  socket.emit("status", "you just got the emit msg from socket.io");
+  socket.on("queue", (queueStatus) => {
+    console.log("queueStatus", queueStatus);
+    io.emit("updatedQueue", queueStatus);
+  });
+});
+
+// connecting to server
+server.listen(PORT, () => {
+  console.log(`listening in port ${PORT}`);
+});
